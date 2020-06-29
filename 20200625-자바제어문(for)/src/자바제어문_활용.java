a42969b181224aba3975624d8bf882e5de71bb81
shhong94 <shhong9@naver.com>
2020-06-29 11:48:03 +0900

    수정

--- 20200625-자바제어문(for)/src/자바제어문_활용.java
+++ 20200625-자바제어문(for)/src/자바제어문_활용.java
@@ -1,6 +1,5 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+
+
 /*
  *  <div class="info_tit">
 	<em class="ico_movie ico_15rating">15세이상관람가</em>
@@ -10,17 +9,9 @@ import org.jsoup.select.Elements;
 public class 자바제어문_활용 {
 
 	public static void main(String[] args) throws Exception{
-		Document doc = Jsoup.connect("https://movie.daum.net/premovie/released").get();
-		System.out.println(doc);
-		Elements title = doc.select("div.info_tit a.name_movie");
-		Elements grade = doc.select("div.info_tit em");
-		for(int i = 0; i < title.size(); i++) {
-			System.out.println("===================");
-			System.out.println(title.get(i).text());
-			System.out.println(grade.get(i).text());
-			System.out.println("===================");
+		
 		}
 
 	}
 
-}
+
---
+++ 20200625-자바제어문(for)2/src/반복문_for문제들.java
@@ -0,0 +1,55 @@
+/*
+ * AaBbCc......
+ */
+public class 반복문_for문제들 {
+
+	public static void main(String[] args) {
+		// AaBbCc......
+		char c = 'a';
+		for(char ch = 'A'; ch <='Z'; ch++) {
+			System.out.print(ch + "" + c++);
+		}
+		System.out.println();
+		System.out.println();
+		
+		// 조건문 없이 1~10까지 홀수만 출력
+		// 일정간격
+		for(int i = 1; i <= 10; i += 2) {
+			System.out.print(i + "\t");
+		}
+		System.out.println();
+		System.out.println();
+		
+		// 조건문 없이 1~10까지 짝수만 출력
+		// 일정간격
+		for(int i = 2; i <= 10; i += 2) {
+			System.out.print(i + "\t");
+		}
+		System.out.println();
+		
+		// 1~5 까지 콤마 포함해서 출력(5다음은 콤마 X)  =>  1,2,3,4,5
+		for(int i = 1; i <= 5; i++) {
+			System.out.print(i);
+			if(i < 5) {
+				System.out.print(",");
+			}
+		}
+		System.out.println();
+		System.out.println();
+		
+		// 이진법
+		// 10 => 1010,   11 => 1111
+		int su = 10;
+		for(;;) {
+			int a = su % 2;
+			System.out.print(a);
+			su = su / 2;
+			if(su == 0) {
+				break;
+			}
+			
+		}
+		System.out.println();
+	}
+
+}
