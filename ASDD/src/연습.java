0fc9c43f99f3dd5edace2bc5ef45ae2f3d24c7aa
shhong94 <63003560+shhong94@users.noreply.github.com>
2020-07-02 21:45:36 +0900

    수정

--- ASDD/src/연습.java
+++ ASDD/src/연습.java
@@ -6,7 +6,21 @@ import javax.swing.*;
 import java.util.*;
 public class ¿¬½À {
 	public static void main(String[] args) {
-		
-		
+/*
+ *  54321
+ *   5432
+ *    543
+ *     54
+ *      5
+ */
+		for(int a = 1; a <= 5; a++){
+			for(int b = 1; b <= a-1; b++) {
+				System.out.print(" ");
+			}
+			for(int c = 5; c >= a; c--) {
+				System.out.print(c);
+			}
+			System.out.println();
+		}
 	}
 }
